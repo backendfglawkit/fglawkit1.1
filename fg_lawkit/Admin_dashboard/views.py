930362feff85e6612f9ad5ddf483e95f18d7be32
backendfglawkit <145880419+backendfglawkit@users.noreply.github.com>
2023-10-17 16:33:18 +0530

    Update views.py

--- fg_lawkit/Admin_dashboard/views.py
+++ fg_lawkit/Admin_dashboard/views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint,render_template,abort
+from flask import Blueprint,render_template,abort,Flask, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from fg_lawkit import db
+from flask_mail import Mail, Message
 
 Admin_dashboard_blueprint = Blueprint('Admin_dashboard', __name__, template_folder='templates/Admin_dashboard')
 
+#for testing only
+app.config['MAIL_SERVER']='live.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'api'
+app.config['MAIL_PASSWORD'] = 'fb600527ba148033335e46c408ba6971'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 ## checking ###
 @Admin_dashboard_blueprint.before_request
@@ -22,3 +30,13 @@ def dashboard_admin():
 @Admin_dashboard_blueprint.route('/analytics')
 def analytics():
         return render_template('analytics.html')
+
+@Admin_dashboard_blueprint.route('/send')
+def send():
+    message=Message(
+        subject="hello",
+        recipients=['backendfglawkit@gmail.com'],
+        sender='backendfglawkit@gmail.com'
+        )
+    message.body='hello yes'
+    return "message sents!"
